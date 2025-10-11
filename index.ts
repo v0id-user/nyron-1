6b1d025541016057c7e2f77da4c03960a93cc2e1
#V0ID <hey@v0id.me>
2025-10-12 01:09:11 +0300

    chore: remove unused index.ts and nyron.config.ts files

--- index.ts
+++
@@ -1,8 +0,0 @@
-export function doWork() {
-  // Simulate doing some other work: multiply numbers from 1 to 5
-  let result = 1;
-  for (let i = 1; i <= 5; i++) {
-    result *= i;
-  }
-  return `Other work done! Product: ${result}`;
-}
